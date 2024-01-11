d6e2775b35fd71bc1fa196a8ee094de838852f59
metzevandro <metzxevandro@gmail.com>
2024-01-11 17:57:20 -0300

    add data table stories variations

--- src/stories/DataTabel.stories.tsx
+++ src/stories/DataTabel.stories.tsx
@@ -13,31 +13,84 @@ const meta: Meta = {
 
 export default meta;
 
-type DefaultProps = {};
+type DefaultProps = {
+  contentPrimaryButton: string;
+  contentSecondaryButton: string;
+  expandable: boolean;
+  selectable: boolean;
+};
 
 const Template: Story<DefaultProps> = (args) => {
   const data = [
-    { id: "1", name: "Olivia Johnson", age: 56 },
-    { id: "2", name: "Ethan Parker", age: 45 },
-    { id: "3", name: "Lily Thompson", age: 30 },
-    { id: "4", name: "Jackson Mitchell", age: 25 },
-    { id: "5", name: "Ava Turner", age: 24 },
-    { id: "6", name: "Noah Bennett", age: 45 },
-    { id: "7", name: "Sophia Hayes", age: 32 },
-    { id: "8", name: "Liam Anderson", age: 21 },
+    {
+      id: "1",
+      name: "Olivia Johnson",
+      age: 56,
+      position: "Manager",
+      department: "HR",
+    },
+    {
+      id: "2",
+      name: "Ethan Parker",
+      age: 45,
+      position: "Developer",
+      department: "Engineering",
+    },
+    {
+      id: "3",
+      name: "Lily Thompson",
+      age: 30,
+      position: "Designer",
+      department: "Creative",
+    },
+    {
+      id: "4",
+      name: "Jackson Mitchell",
+      age: 25,
+      position: "Intern",
+      department: "IT",
+    },
+    {
+      id: "5",
+      name: "Ava Turner",
+      age: 24,
+      position: "Analyst",
+      department: "Finance",
+    },
+    {
+      id: "6",
+      name: "Noah Bennett",
+      age: 45,
+      position: "Manager",
+      department: "Sales",
+    },
+    {
+      id: "7",
+      name: "Sophia Hayes",
+      age: 32,
+      position: "Engineer",
+      department: "Engineering",
+    },
+    {
+      id: "8",
+      name: "Liam Anderson",
+      age: 21,
+      position: "Intern",
+      department: "IT",
+    },
   ];
 
-  const columns = ["name", "age"];
+  const columns = ["name", "age", "position", "department"];
 
   const expandedData = [
-    { id: "1", name: "Experience: 20 years" },
-    { id: "2", name: "Experience: 12 years" },
-    { id: "3", name: "Experience: 12 years" },
-    { id: "4", name: "Experience: 5 years" },
-    { id: "5", name: "Experience: 2 years" },
-    { id: "6", name: "Experience: 23 years" },
-    { id: "7", name: "Experience: 7 years" },
-    { id: "8", name: "Experience: 1 years" },
+    { id: "1", content: "Experience: 20 years", context: "Projects: 50" },
+    { id: "2", content: "Experience: 15 years", context: "Projects: 40" },
+    { id: "3", content: "Experience: 18 years", context: "Projects: 35" },
+    { id: "4", content: "Experience: 10 years", context: "Projects: 25" },
+    { id: "5", content: "Experience: 22 years", context: "Projects: 60" },
+    { id: "6", content: "Experience: 14 years", context: "Projects: 28" },
+    { id: "7", content: "Experience: 17 years", context: "Projects: 45" },
+    { id: "8", content: "Experience: 19 years", context: "Projects: 55" },
   ];
 
   return (
@@ -45,15 +98,44 @@ const Template: Story<DefaultProps> = (args) => {
       <DataTable
         columns={columns}
         data={data}
-        contentPrimaryButton="Button"
-        contentSecondButton="Button"
-        expandable={true}
-        selectable={true}
+        contentPrimaryButton={args.contentPrimaryButton}
+        contentSecondButton={args.contentSecondaryButton}
         expandedData={expandedData}
+        expandable={args.expandable}
+        selectable={args.selectable}
       />
     </>
   );
 };
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  contentPrimaryButton: "button",
+  contentSecondaryButton: "button",
+  expandable: false,
+  selectable: false,
+};
+
+export const WithSelectable = Template.bind({});
+WithSelectable.args = {
+  contentPrimaryButton: "button",
+  contentSecondaryButton: "button",
+  expandable: false,
+  selectable: true,
+};
+
+export const WithExpandable = Template.bind({});
+WithExpandable.args = {
+  contentPrimaryButton: "button",
+  contentSecondaryButton: "button",
+  expandable: true,
+  selectable: false,
+};
+
+export const With_Expandable_and_Selectable = Template.bind({});
+With_Expandable_and_Selectable.args = {
+  contentPrimaryButton: "button",
+  contentSecondaryButton: "button",
+  expandable: true,
+  selectable: true,
+};
