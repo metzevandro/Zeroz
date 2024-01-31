588d8786e1335ea785b6f14cf9acf7bada624eaa
metzevandro <metzxevandro@gmail.com>
2024-01-30 22:47:14 -0300

    add args to each one of the progress indicator

--- src/stories/ProgressIndicator.stories.tsx
+++ src/stories/ProgressIndicator.stories.tsx
@@ -16,6 +16,21 @@ export default meta;
 
 type Args = {
   direction: "row" | "column";
+  state1: 'default' | 'current' | 'completed' | 'error' | 'disable'
+  state2: 'default' | 'current' | 'completed' | 'error' | 'disable'
+  state3: 'default' | 'current' | 'completed' | 'error' | 'disable'
+  state4: 'default' | 'current' | 'completed' | 'error' | 'disable'
+  state5: 'default' | 'current' | 'completed' | 'error' | 'disable'
+  step1: string;
+  step2: string;
+  step3: string;
+  step4: string;
+  step5: string;
+  description1: string;
+  description2: string;
+  description3: string;
+  description4: string;
+  description5: string;
 };
 
 const Template: Story<Args> = (args) => {
@@ -23,33 +38,33 @@ const Template: Story<Args> = (args) => {
     <div style={{ display: "flex", flexDirection: args.direction }}>
       <ProgressIndicator
         direction={args.direction}
-        state="completed"
-        step="Step 1"
-        description="Description"
+        state={args.state1}
+        step={args.step1}
+        description={args.description1}
       />
       <ProgressIndicator
         direction={args.direction}
-        state="current"
-        step="Step 2"
-        description="Description"
+        state={args.state2}
+        step={args.step2}
+        description={args.description2}
       />
       <ProgressIndicator
         direction={args.direction}
-        state="default"
-        step="Step 3"
-        description="Description"
+        state={args.state3}
+        step={args.step3}
+        description={args.description3}
       />
       <ProgressIndicator
         direction={args.direction}
-        state="disable"
-        step="Step 4"
-        description="Description"
+        state={args.state4}
+        step={args.step4}
+        description={args.description4}
       />
       <ProgressIndicator
         direction={args.direction}
-        state="error"
-        step="Step 5"
-        description="Description"
+        state={args.state5}
+        step={args.step5}
+        description={args.description5}
       />
     </div>
   );
@@ -58,4 +73,19 @@ const Template: Story<Args> = (args) => {
 export const Default = Template.bind({});
 Default.args = {
   direction: "row",
+  step1: 'Step 1',
+  description1: 'Description 1',
+  state1: 'completed',
+  step2: 'Step 2',
+  description2: 'Description 2',
+  state2: 'current',
+  step3: 'Step 3',
+  description3: 'Description 3',
+  state3: 'default',
+  step4: 'Step 4',
+  description4: 'Description 4',
+  state4: 'disable',
+  step5: 'Step 5',
+  description5: 'Description 5',
+  state5: 'error',
 };
