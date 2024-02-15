a186277127d8cd27ea656a5e0a44adf5cb43dec7
metzevandro <metzxevandro@gmail.com>
2024-02-15 16:39:30 -0300

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   webpack: (config) => {
     config.resolve.alias['@components'] = path.join(__dirname, 'src/components');
+    config.resolve.alias['@scss'] = path.join(__dirname, 'src/app/scss'); // Adicionando o alias para a pasta scss
     return config;
   },
 };
