e936271fcda18344cd6565db6783422ad2409deb
metzevandro <metzxevandro@gmail.com>
2024-02-15 16:09:09 -0300

    [FIX] problem with package

--- next.config.js
+++ next.config.js
@@ -1,26 +1,8 @@
 const path = require('path');
-const fs = require('fs');
-
-function getComponentAliases() {
-  const componentsDir = path.join(__dirname, 'src/components');
-  const componentFolders = fs.readdirSync(componentsDir);
-
-  const aliases = {};
-  componentFolders.forEach(folder => {
-    const folderPath = path.join(componentsDir, folder);
-    const isDirectory = fs.statSync(folderPath).isDirectory();
-    if (isDirectory) {
-      aliases[`@components/${folder}`] = path.join(folderPath, 'index.tsx');
-    }
-  });
-
-  return aliases;
-}
 
 module.exports = {
   webpack: (config) => {
-    const aliases = getComponentAliases();
-    config.resolve.alias = { ...config.resolve.alias, ...aliases };
+    config.resolve.alias['@zeroz'] = path.join(__dirname, 'src/components');
     return config;
   },
 };
