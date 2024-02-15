2f745f4879814b8c9849a99cb7c869579865b2d9
metzevandro <metzxevandro@gmail.com>
2024-02-15 15:57:56 -0300

    Commit das alterações para a nova versão do pacote

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+const fs = require('fs');
 
-module.exports = nextConfig
+function getComponentAliases() {
+  const componentsDir = path.join(__dirname, 'src/components');
+  const componentFolders = fs.readdirSync(componentsDir);
+
+  const aliases = {};
+  componentFolders.forEach(folder => {
+    const folderPath = path.join(componentsDir, folder);
+    const isDirectory = fs.statSync(folderPath).isDirectory();
+    if (isDirectory) {
+      aliases[`@components/${folder}`] = path.join(folderPath, 'index.tsx');
+    }
+  });
+
+  return aliases;
+}
+
+module.exports = {
+  webpack: (config) => {
+    const aliases = getComponentAliases();
+    config.resolve.alias = { ...config.resolve.alias, ...aliases };
+    return config;
+  },
+};
