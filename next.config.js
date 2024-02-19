6703f3a815fe6686eab7e002ce0656daaf9f43fb
metzevandro <metzxevandro@gmail.com>
2024-02-19 14:43:04 -0300

    [TESTE]

--- next.config.js
+++ next.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const withTM = require('next-transpile-modules')(['@design-system-zeroz']);
 
-module.exports = {
-  // Outras configurações...
-  resolve: {
-    alias: {
-      '@design-system-zeroz': path.resolve(__dirname, 'src/app/components')
-    }
-  }
-};
+module.exports = withTM({
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      ],
+    });
+
+    // Adicionar alias para o seu pacote de design system
+    config.resolve.alias['@design-system-zeroz'] = path.resolve(__dirname, 'src/app/components');
+
+    return config;
+  },
+});
