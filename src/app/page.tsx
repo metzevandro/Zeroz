c51f8f1936650659d41a0281aa83a71077b8e719
metzevandro <metzxevandro@gmail.com>
2024-02-19 15:19:52 -0300

    [UPDATE] next package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,44 @@
 "use client";
 
+import { useState } from "react";
+import ColorPicker from "./components/ColorPicker/ColorPicker";
+type FormValuesType = {
+  [key: string]:
+    | string
+    | boolean
+    | number
+    | File[]
+    | FileList
+    | null
+    | undefined;
+};
 export default function Home() {
-  return;
+  const [formValues, setFormValues] = useState<FormValuesType>({
+    Input: "",
+    "Input Select": "Option 1",
+    "Input Number": "0",
+    ImageUploader: null,
+    FileUploader: null,
+    Checkbox: false,
+    Switch: false,
+    "Radio Button": false,
+    "Input do DataPicker": "",
+    ColorPicker: "#FFFFFF",
+    Slider: "0",
+    "Input Time": "",
+    "Text Area": "",
+  });
+  const handleColorChange = (color: string) => {
+    setFormValues((prevFormValues) => ({
+      ...prevFormValues,
+      ColorPicker: color,
+    }));
+  };
+  return (
+    <ColorPicker
+                label="Color Picker"
+                value={String(formValues.ColorPicker)}
+                onChange={handleColorChange}
+              />
+  );
 }
