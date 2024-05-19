df2ed5ffed9a9c1613e0a0b86b9fd1aa59e4a5d8
metzevandro <metzxevandro@gmail.com>
2024-05-19 15:56:42 -0300

    [UPDATE] the storybook and [FIX] the problem with cursor in disable button

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,20 +6,27 @@ const config: StorybookConfig = {
     "../src/**/*/*.mdx",
     "../src/**/*/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
+
   staticDirs: ["../public"],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-mdx-gfm",
     "@storybook/addon-a11y",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: "@storybook/nextjs",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
