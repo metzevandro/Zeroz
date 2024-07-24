88fbba80d7ce0dd68cd8480acd5e83997b3aa0e0
metzevandro <metzxevandro@gmail.com>
2024-07-23 22:05:52 -0300

    [TESTE]

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 
 module.exports = {
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.alias["@design-system-zeroz"] = path.resolve(
       __dirname,
-      "src/app/components",
+      "dist/src/app/components",
     );
 
     return config;
