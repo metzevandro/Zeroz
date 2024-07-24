9641d4479559993dd7236faef898f5dcd0eb82e0
metzevandro <metzxevandro@gmail.com>
2024-07-23 21:20:13 -0300

    [TESTE]

--- next.config.js
+++ next.config.js
@@ -1,33 +1,12 @@
 const path = require("path");
-const withTM = require('next-transpile-modules')(['design-system-zeroz']); 
 
-const nextConfig = withTM({
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.tsx?$/,
-      include: [
-        path.resolve(__dirname, "node_modules/design-system-zeroz"),
-        path.resolve(__dirname, "src"),
-      ],
-      use: [
-        {
-          loader: "ts-loader",
-          options: {
-            allowTsInNodeModules: true,
-            transpileOnly: true, 
-          },
-        },
-      ],
-      exclude: /node_modules\/(?!design-system-zeroz)/,
-    });
-
+module.exports = {
+  webpack: (config, { isServer }) => {
     config.resolve.alias["@design-system-zeroz"] = path.resolve(
       __dirname,
-      "node_modules/design-system-zeroz/src/app/components"
+      "src/app/components",
     );
 
     return config;
   },
-});
-
-module.exports = nextConfig;
+};
