d0a8947ee9bf9ef6a3f07d1acc63017c0845875d
metzevandro <metzxevandro@gmail.com>
2024-07-23 21:42:25 -0300

    [TESTE]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./styles.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import React from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
