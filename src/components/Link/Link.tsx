4c15047906571c630f581050562e70146e51b7cf
metzevandro <metzxevandro@gmail.com>
2026-06-16 21:38:22 -0300

    feat: adiciona estado de Skeleton para o Link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,24 +1,49 @@
-import "./Link.scss";
-import React from "react";
-
-interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-  href?: string;
-  disabled?: boolean;
-  target?: "_blank" | "_self" | "_parent" | "_top";
-  children: React.ReactNode;
-}
+import { useLayoutEffect, useRef, useState } from "react";
+import { LinkProps } from "./Link.types";
+import { Skeleton } from "../Skeleton";
+import './Link.scss'
 
 const Link: React.FC<LinkProps> = ({
   content,
   disabled,
   children,
+  skeleton,
   ...rest
 }) => {
+  const linkRef = useRef<HTMLAnchorElement>(null);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  useLayoutEffect(() => {
+    if (linkRef.current) {
+      const { offsetWidth, offsetHeight } = linkRef.current;
+      setDimensions({
+        width: offsetWidth,
+        height: offsetHeight,
+      });
+    }
+  }, [children, disabled, skeleton]);
+
   return (
     <>
-      <a className={`link ${disabled ? "disabled" : ""}`} {...rest}>
+      <a
+        ref={linkRef}
+        className={`link ${disabled ? "disabled" : ""}`}
+        style={{
+          position: skeleton ? "fixed" : "static",
+          visibility: skeleton ? "hidden" : "visible",
+          left: skeleton ? "-9999px" : "auto",
+          ...(rest.style || {}),
+        }}
+        {...rest}
+      >
         {children}
       </a>
+      {skeleton && (
+        <Skeleton
+          width={`${dimensions.width}px`}
+          height={`${dimensions.height}px`}
+        />
+      )}
     </>
   );
 };
