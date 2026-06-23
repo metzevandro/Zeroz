ce71527e189067bb117f671d5099358343bb268d
metzevandro <metzxevandro@gmail.com>
2026-06-22 21:40:49 -0300

    fix: ajusta os problemas com a renderização do componente de skeleton em todas as variantes do componente Pagination

--- src/components/Pagination/subcomponents/PaginationArrowButton.tsx
+++ src/components/Pagination/subcomponents/PaginationArrowButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Icon from "../../Icon/Icon";
 import { PaginationArrowButtonProps } from "../Pagination.types";
 import { ARROW_ICON } from "../utils/pagination.utils";
+import { Skeleton } from "../../Skeleton";
 
 /**
  * Renders a single directional arrow button for the `Pagination` component.
@@ -10,15 +11,20 @@ export function PaginationArrowButton({
   direction,
   disabled,
   onClick,
+  skeleton,
 }: PaginationArrowButtonProps) {
   const icon = ARROW_ICON[direction];
+
+  if (skeleton) {
+    return <Skeleton width="33px" height="33px"></Skeleton>;
+  }
 
   return (
     <button
       className={icon}
       disabled={disabled}
       onClick={onClick}
-      aria-label={direction === "left" ? "Previous page" : "Next page"}
+      aria-label={direction === "left" ? "Previous" : "Next"}
     >
       <Icon icon={icon} size="sm" />
     </button>
